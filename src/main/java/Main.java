c9c2ee2edbbd0a2740cd51bd9b3d740dd380d2dd
Glenfix <glenfixchannel@gmail.com>
2024-05-25 11:31:09 +0300

    Fix bugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
-
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Здравствуйте, на сколько человек делим счёт? (введите число):");
+        Positions.theQuestion();
+        Positions.addPositionAndPrice();
+        Positions.continueAddPositions();
+        Positions.cheque();
+        Positions.conclusion();
     }
 }
